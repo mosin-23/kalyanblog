dce5e56e94aa9ba32055b9d2733a2f87b983cb9c
Abdulmosinkhanpathan <114325519+Abdulmosinkhanpathan@users.noreply.github.com>
2025-06-10 22:40:30 +0530

    footer modified

--- client/kalyan/src/Footer.js
+++ client/kalyan/src/Footer.js
@@ -105,8 +105,7 @@ const Footer = () => {
                   {[
                     { name: 'Home', href: '/' },
                     { name: 'About', href: '/about' },
-                    { name: 'Disclaimer', href: '/disclaimer' },
-                    { name: 'Blog', href: '/about' },
+                    { name: 'Privacy Policy', href: '/privacy-policy"' },
                     { name: 'Admin', href: '/admin/login' }
                   ].map((item) => (
                     <li key={item.name}>
